dc5bc0eadbef78014b3ce343096aab2fd2947db7
juancr5 <37746221+juancr5@users.noreply.github.com>
2019-10-08 20:52:41 -0500

    Update mi_ls.c

--- mi_ls.c
+++ mi_ls.c
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
         {
             sprintf(buf, "%s/%s", argv[1], miArchivo->d_name);    
             char archivo[100];
-            strcpy(archivo, "./punto1.out ");
+            strcpy(archivo, "./fstat.out");
             strcat(archivo, miArchivo->d_name);
             int status = system(&archivo);
         }
